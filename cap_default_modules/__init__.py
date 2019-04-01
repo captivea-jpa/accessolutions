c7120e52d4bfeef882c63245772dbbd6faf2be5c
captivea-jpa <35844968+captivea-jpa@users.noreply.github.com>
2019-04-01 16:11:51 +0200

    #18709 add pre_init_hook.

--- cap_default_modules/__init__.py
+++ cap_default_modules/__init__.py
@@ -1,2 +1,9 @@
 # coding: utf-8
 # Part of CAPTIVEA. Odoo 11.
+
+__all__ = []
+
+
+def pre_init_hook(cr):
+  """Called before module installation."""
+  cr.execute("UPDATE ir_module_module SET state = 'to upgrade' WHERE name = 'document';")
