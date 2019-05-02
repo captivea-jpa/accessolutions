16d10e9aaef0dffea1cbd36fe05dcb80137c4a6e
captivea-jpa <35844968+captivea-jpa@users.noreply.github.com>
2019-05-02 18:33:05 +0200

    Update __init__.py

--- cap_default_modules/__init__.py
+++ cap_default_modules/__init__.py
@@ -2,8 +2,3 @@
 # Part of CAPTIVEA. Odoo 11.
 
 __all__ = []
-
-
-def post_init_hook(cr, registry):
-  """Called after module installation."""
-  cr.execute("UPDATE ir_module_module SET state = 'to install' WHERE name = 'document';")
